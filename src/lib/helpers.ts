5071e278a57387a33cf9220d7e2f1b5a7e78cad4
Daniel Lennox <lennox.dan@gmail.com>
2020-07-31 18:36:32 +1000

    Add sourcemap to tsconfig.

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -10,7 +10,7 @@
  * @param array
  *   The array to shuffle.
  */
-export function shuffleArray(array) {
+export function shuffleArray(array: Array<any>) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
