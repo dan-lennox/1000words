8f761162267a00a4e579bb8cd2c148d642f00d76
Daniel Lennox <lennox.dan@gmail.com>
2020-11-09 12:09:53 +1100

    Replace word count slider with google mdc-components version.

--- src/components/WordCountSlider.tsx
+++ src/components/WordCountSlider.tsx
@@ -1,7 +1,7 @@
 /**
  * WordCountSlider Component.
  */
-import { createRef, ReactElement, useEffect, useRef, RefObject } from 'react';
+import { createRef, ReactElement, useEffect, useRef, RefObject, useState } from 'react';
 import { MDCSlider } from '@material/slider';
 
 type Props = {
@@ -10,50 +10,51 @@ type Props = {
 };
 
 const WordCountSlider = ({ wordCount, update }: Props): ReactElement => {
-  const sliderRef: RefObject<HTMLDivElement> = createRef<HTMLDivElement>();
+  const sliderElementRef: RefObject<HTMLDivElement> = createRef<HTMLDivElement>();
+  const [slider, setSlider] = useState<any>(undefined);
 
   useEffect(() => {
-    const slider = new MDCSlider(sliderRef.current as HTMLDivElement);
-    slider.listen('MDCSlider:change', () => update(slider.value));
+    if (!slider) {
+      let mdcSlider = new MDCSlider(sliderElementRef.current as HTMLDivElement);
+
+      mdcSlider.listen(
+        'MDCSlider:change',
+        (event: CustomEvent) => {
+          if (event.detail.value !== wordCount) {
+            update(event.detail.value);
+          }
+        },
+        slider
+      );
+
+      setSlider(mdcSlider);
+    }
   });
 
   return (
     <div
-      ref={sliderRef}
+      ref={sliderElementRef}
       className="mdc-slider mdc-slider--discrete"
       tabIndex={0}
       role="slider"
       aria-valuemin={0}
-      aria-valuemax={100}
-      aria-valuenow={0}
+      aria-valuemax={1000}
+      aria-valuenow={wordCount}
       aria-label="Select Value"
     >
       <div className="mdc-slider__track-container">
-        <div className="mdc-slider__track"></div>
+        <div className="mdc-slider__track" />
       </div>
       <div className="mdc-slider__thumb-container">
         <div className="mdc-slider__pin">
-          <span className="mdc-slider__pin-value-marker"></span>
+          <span className="mdc-slider__pin-value-marker" />
         </div>
         <svg className="mdc-slider__thumb" width="21" height="21">
-          <circle cx="10.5" cy="10.5" r="7.875"></circle>
+          <circle cx="10.5" cy="10.5" r="7.875" />
         </svg>
-        <div className="mdc-slider__focus-ring"></div>
+        <div className="mdc-slider__focus-ring" />
       </div>
     </div>
-    // <form action="#">
-    //   <p className="range-field">
-    //     <input
-    //       type="range"
-    //       className="word-count-range"
-    //       min="20"
-    //       max="1000"
-    //       step="20"
-    //       defaultValue={wordCount}
-    //       onChange={(e) => update(parseInt(e.target.value))}
-    //     />
-    //   </p>
-    // </form>
   );
 };
 export default WordCountSlider;
